0c46e01af6cb85bef6624974606d082c86e2ef2f
Ahmed Rashid <98060670+ahmedsayidrashid@users.noreply.github.com>
2024-08-09 15:36:18 -0400

    ps2 interface added to source code

--- Interface.py
+++ Interface.py
@@ -1,18 +1,20 @@
 from machine import Pin
 import time
 
-# Define address and data pins
+# Define address and data pins for the SRAM
 address_pins = [Pin(i, Pin.OUT) for i in range(13)] 
 data_pins = [Pin(i, Pin.OUT) for i in [17, 18, 19, 20, 21, 22, 26, 27]]  
-
 ReadOrWrite = Pin(16, Pin.OUT)  # Read/Write control
+
+# PS2 Keyboard definitions
+ps2_data_pin = Pin(15, Pin.IN, Pin.PULL_UP)
+clock_pin = Pin(28, Pin.IN, Pin.PULL_UP)
 
 def set_address(address):
     for i in range(13):
         address_pins[i].value((address >> i) & 1)
-        #print("address bit number " + str(i) + ": value: " + str(address_pins[i].value()))
 
-def configure_data_pins_as_input(): # Function to configure data pins as inputs
+def configure_data_pins_as_input(): 
     for pin in data_pins:
         pin.init(Pin.IN)
 
@@ -32,7 +34,6 @@ def read_sram(address):
     data = 0
     for i in range(8):  # Reading 8-bit data
         data |= (data_pins[i].value() << i)
-        #print("data bit number " + str(i) + ": value: " + str(data_pins[i].value()))
     return data
 
 # Function to write data to SRAM
@@ -46,33 +47,68 @@ def write_sram(address, data):
     for i in range(8):
         data_pins[i].value((data >> i) & 1)
     time.sleep(0.01)  # Allow time for data to be written
- 
-# Function to output all the SRAM data
-def read_all_sram(size):
-    for address in range(size):
-        data = read_sram(address)
-        print(f"Address {address:04X}: Data {data:02X}")
 
 # Function to write a specific location in memory.
 def write_some_sram(start_loc, end_loc, data):
     if(end_loc < start_loc):
         print("End location must be greater")
         return
-    for address in range(start_loc,end_loc+1):
+    for address in range(start_loc, end_loc + 1):
         write_sram(address, data)
+        
+# Function to output all the SRAM data
+def read_all_sram(size):
+    for address in range(size + 1):
+        data = read_sram(address)
         print(f"Address {address:04X}: Data {data:02X}")
 
-# Example usage
-address_to_write = 0x0000
-data_to_write = 0xAB
-
-write_sram(address_to_write, data_to_write)
-time.sleep(0.1)  # Wait before reading
-
-# Read first 10 addresses of SRAM
-read_all_sram(10)
-
-# Ideal case should be to output 0xAB for address 0x00 and 0x00 for the rest of addresses.
-
-
+# Function to initialize the entire sram with data.
+def initialize_all_sram(data): # usually data = 0x00 to reset the SRAM
+    size = 64 * 1024
+    for address in range(size):
+        write_sram(address, data)
+        
+def print_sram_data(address):
+    for num in range(address+1):
+        data = hex(read_sram(num))[2:].upper()
+        print("Address: " + str(num) + " Data: 0X" + str(data))
+    print("-----") # for format purposes
     
+# Function to read data from the PS/2 keyboard.
+def read_ps2_keyboard():
+    while True:
+        if not clock_pin.value():  # Wait for a clock pulse (low signal)
+            bitstream = 0
+            # Read data bits
+            for i in range(8):
+                while clock_pin.value():  # Wait for clock to go low
+                    pass
+                bitstream |= (ps2_data_pin.value() << i)
+                while not clock_pin.value():  # Wait for clock to go high
+                    pass
+            time.sleep(0.1) # some delay to be cautious
+            
+            #bitstream = hex(bitstream)[2:].upper() # convert the data to hex
+            return bitstream
+        
+pc = 0 # program counter
+data_to_sram = 0x00
+
+while(1):
+    data_to_sram = read_ps2_keyboard()
+    
+    if(data_to_sram == 0xEC): # Escape key resets the entire sram
+        write_some_sram(0, pc, 0x00)
+        pc = 0
+        print("SRAM has been reset")
+    elif(data_to_sram == 0x52): # Space key exits the program
+        print("--- Displaying memory contents ---")
+        read_all_sram(pc)
+    elif(data_to_sram == 0xCC): # Backspace (<-) key exits the program
+        print("Exiting the program...")
+        time.sleep(1)
+        break
+    else:
+        write_sram(pc, data_to_sram) # write the raw keyboard input into the sram
+        pc += 1 #increment the address in memory to write to
+        print("Address " + str(pc) + " populated with keypress ASCII code")
